ed95f3991f080a7fc0b26239a45e07b6ed4702b9
fnematov <mr.nematov91@gmail.com>
2021-11-22 15:47:47 +0500

    route correcting

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,19 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '../views/Home.vue'
+import Users from '../views/Users.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Users',
+    component: Users
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/companies',
+    name: 'Companies',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Companies.vue')
   }
 ]
 
